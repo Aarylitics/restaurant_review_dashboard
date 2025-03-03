eaf26c8bba6676ddb650cec22b19e1aaee1bb191
Aary <71329841+Aarylitics@users.noreply.github.com>
2025-03-03 11:28:37 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -49,12 +49,12 @@ from selenium.webdriver.chrome.options import Options
 st.set_page_config(
     page_title='Restaurant Review Dashboard',
     layout="wide",
-    initial_sidebar_state="expanded",
-    page_icon="IMG_1109.png"    
+    initial_sidebar_state="expanded"#,
+    #page_icon="IMG_1109.png"    
 )
 
 #set sidebar
-st.sidebar.image("IMG_1109.png", use_container_width=True)
+#st.sidebar.image("IMG_1109.png", use_container_width=True)
 st.sidebar.title('Restaurant Review Dashboard')
 st.sidebar.divider()
 menu = st.sidebar.selectbox("Select Analysis Section", ["Home", "Word Analysis", "Topic Clustering", "Mentions", "What to Expect"])

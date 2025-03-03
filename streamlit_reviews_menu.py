5478da6c4d38eb80ed17192c0476a0fb325a5918
Aary <71329841+Aarylitics@users.noreply.github.com>
2025-03-03 10:57:08 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -80,7 +80,7 @@ if menu == "Home":
 @st.cache_data
 ####### PARSE REVIEWS#####
 
-#@st.experimental_singleton
+@st.experimental_memo
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 

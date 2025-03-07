a3144ebf06219eec2ff31a42f134e9ef171b3064
Aary <71329841+Aarylitics@users.noreply.github.com>
2025-03-07 13:52:15 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -95,7 +95,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Define options BEFORE using it
 options = Options()
 options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+#options.add_argument('--headless')
 options.add_argument("--no-sandbox")  # Bypass OS security model
 options.add_argument('--disable-extensions')
 options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems

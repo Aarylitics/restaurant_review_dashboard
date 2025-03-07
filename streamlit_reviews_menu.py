72e2320a8e0d1019cf5fe05d283dd4d44b3fd4eb
Aary <71329841+Aarylitics@users.noreply.github.com>
2025-03-07 15:03:22 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -93,38 +93,43 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-
-
-
-# def get_driver():
-#     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-# # Now call the function
-
-driver = webdriver.Chrome()
-
-
-
-# def get_driver():
-#      # Define options BEFORE using it
-#      options = Options()
-#      options.add_argument('--disable-gpu')
-#      #options.add_argument('--headless')
-#      options.add_argument("--no-sandbox")  # Bypass OS security model
-#      options.add_argument('--disable-extensions')
-#      options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-#      return webdriver.Chrome(
-#         service=Service(
-#             ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-#         ),
-#         options=options,
-#     )
-
-# driver = get_driver()
 @st.cache_resource
 
+
+def get_website_content(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+        time.sleep(5)
+        html_doc = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup.get_text()
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
+
 def scrape_data():
+     driver = None
+     try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
 
     driver.get(url)
 

013eba65daf6ef6db988c7a2cb6f886c1cce74b3
Aary <71329841+Aarylitics@users.noreply.github.com>
2025-03-07 14:23:36 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -92,13 +92,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Define options BEFORE using it
-options = Options()
-options.add_argument('--disable-gpu')
-#options.add_argument('--headless')
-options.add_argument("--no-sandbox")  # Bypass OS security model
-options.add_argument('--disable-extensions')
-options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
 
 @st.cache_resource
 # def get_driver():
@@ -107,8 +101,17 @@ options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource pro
 # # Now call the function
 # driver = webdriver.Chrome()
 
+
+
 def get_driver():
-    return webdriver.Chrome(
+     # Define options BEFORE using it
+     options = Options()
+     options.add_argument('--disable-gpu')
+     #options.add_argument('--headless')
+     options.add_argument("--no-sandbox")  # Bypass OS security model
+     options.add_argument('--disable-extensions')
+     options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+     return webdriver.Chrome(
         service=Service(
             ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
         ),

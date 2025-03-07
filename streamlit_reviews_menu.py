35398fe93d6dfe766618eb509ee78ba6c53a78a9
Aary <71329841+Aarylitics@users.noreply.github.com>
2025-03-07 15:07:57 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -95,6 +95,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @st.cache_resource
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+import time
+import streamlit as st
 
 def get_website_content(url):
     driver = None
@@ -105,105 +113,71 @@ def get_website_content(url):
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1920,1200')
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                  options=options)
+                                    options=options)
         st.write(f"DEBUG:DRIVER:{driver}")
         driver.get(url)
         time.sleep(5)
         html_doc = driver.page_source
-        driver.quit()
-        soup = BeautifulSoup(html_doc, "html.parser")
-        return soup.get_text()
+        return BeautifulSoup(html_doc, "html.parser")
     except Exception as e:
         st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
     finally:
-        if driver is not None: driver.quit()
-    return None
+        if driver is not None:
+            driver.quit()
 
-def scrape_data():
+def scrape_data(url):
     driver = None
-    # Using on Local
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1200')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                options=options)
-    st.write(f"DEBUG:DRIVER:{driver}")
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                    options=options)
+        driver.get(url)
+        time.sleep(3)
 
-    driver.get(url)
+        rest_name = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1').text
+        rest_type = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button').text
+        value = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/span/span/span/span[2]/span/span').text
 
-    #may need to define xpath for "i agree" button. Did not pop up for me, will try on someone elses device later
-    import time
-    time.sleep(3)
+        driver.find_element(By.XPATH, "//button[contains(@aria-label, 'Reviews')]").click()
+        total_rating = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]').text
+        num_reviews = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[3]').text.split(" ")[0]
 
-    #obtain title
-    rest_name = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1').text
+        if "," in num_reviews:
+            num_reviews = num_reviews.replace(",", "")
 
-    #obtain resturant type
-    rest_type = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button').text
+        height = 0
+        while height <= int(num_reviews):
+            try:
+                scroll_element = driver.find_element(By.XPATH, "//*[@id='QA0Szd']/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]")
+                try:
+                    more_element = driver.find_element(By.XPATH, "//button[@aria-label='See more']")
+                    if more_element.get_attribute("aria-expanded") == "false":
+                        more_element.click()
+                        time.sleep(0.25)
+                except NoSuchElementException:
+                    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_element)
+                    time.sleep(0.25)
+            except NoSuchElementException:
+                print("Scrollbar element not found.")
+                break
+            height += 1
 
-    #obtain restaurant value
-    value = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/span/span/span/span[2]/span/span').text
-
-    #get restaurant address
-    #address = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[9]/div[3]/button/div/div[2]/div[1]").text
-    #can create a dictionary, key is state, div number is entry; search for state, if state, then that div number
-
-    #if need to go thru newer reviews first, insert that in here:
-
-    #code chunk below helps us find review button. Got this code off of medium
-    driver.find_element(By.XPATH, "//button[contains(@aria-label, 'Reviews')]").click()
-
-    #obtain rating
-    total_rating = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]').text
-
-    #scroll till all reviews are loaded up
-        #scroll by amount -- calculate and see how many reviews are in one scroll (10 scrolls is in one scroll)
-    num_reviews = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[3]').text.split(" ")[0] #this code gives us the number
-
-    #some reviews may have columns, will want to take that out
-    if num_reviews.find(",") == True:
-        num_reviews = num_reviews.replace(",","")
-    else:
-        num_reviews = num_reviews
-
-    #now that we have number of reviews, we can scroll through reviews and load up each review
-
-    height = 0
-    while height <= (int(num_reviews)):
-        try:
-            scroll_element = driver.find_element(By.XPATH, "//*[@id='QA0Szd']/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]") #want to scroll first; finds scroll bar element
-            try: #find "more"
-                more_element = driver.find_element(By.XPATH, "//button[@aria-label='See more']")
-                if more_element.get_attribute("aria-expanded") == "false":
-                    more_element.click()
-                    time.sleep(.25) #might try (int(num_reviews)/10) (would need to divide by the number of 0's plus 2) len(str(num_reviews)).
-            except NoSuchElementException: #scroll if no "more"
-                driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_element)
-                time.sleep(.25)
-        except NoSuchElementException:
-            print("Scrollbar element not found.")
-            break
-        height += 1 #once height is reached... or it doesnt touch anymore, break
-
-    #acquire reviews and parse them into a dataset #obtained from medium: https://medium.com/@isguzarsezgin/scraping-google-reviews-with-selenium-python-23135ffcc331
-    review = BeautifulSoup(driver.page_source,'html.parser')
-    driver.quit()
-    return rest_name, rest_type, value, total_rating, num_reviews, review
-
-if __name__ == "__main__":
-    review = scrape_data()
-
-    if review:
-        rest_name, rest_type, value, total_rating, num_reviews, reviews = review
-
-        st.sidebar.write(f"**Restaurant Name**: {rest_name}")
-        st.sidebar.write(f"**Restaurant Type**: {rest_type}")
-        st.sidebar.write(f"**Average Value Spend**: {value}")
-        st.sidebar.write(f"**Average Rating (Google)**: {total_rating}")
-        st.sidebar.write(f"**Number of Reviews**: {num_reviews}")
+        review = BeautifulSoup(driver.page_source, 'html.parser')
+        return rest_name, rest_type, value, total_rating, num_reviews, review
+    except Exception as e:
+        st.write(f"DEBUG:SCRAPE_DATA:ERROR:{e}")
+        return None,None,None,None,None,None
+    finally:
+        if driver is not None:
+            driver.quit()
         
 @st.cache_data
+
+rest_name, rest_type, value, total_rating, num_reviews, review = scrape_data(url)
+
 
 #Extract restaurant basic informaton: Name, Address, Number of Reviews, Average Rating, Restaurant Type, Map
 # st.sidebar.write(f"**Restaurant Name**: {rest_name}")

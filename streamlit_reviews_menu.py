7dfc05bfaafb474793415f6fe7db70ccd304e610
Aary <71329841+Aarylitics@users.noreply.github.com>
2026-05-03 22:56:18 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.core.os_manager import ChromeType
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait

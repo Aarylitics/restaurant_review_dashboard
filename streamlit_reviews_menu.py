049539525ddf6c512265b03a5977b52a240ae91b
Aary <71329841+Aarylitics@users.noreply.github.com>
2026-09-04 10:21:38 -0500

    Update streamlit_reviews_menu.py

--- streamlit_reviews_menu.py
+++ streamlit_reviews_menu.py
@@ -8,11 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.core.os_manager import ChromeType
-import time
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import json
 from bs4 import BeautifulSoup
 import numpy as np
@@ -134,10 +129,6 @@ def scrape_data():
     # chrome_options.add_argument('--disable-extensions')
     options.add_argument("--no-sandbox")  # Required for Docker
     options.add_argument("--disable-dev-shm-usage")  # Helps with resource issues
-
-    driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
-    options=options)
     
     temp_dir = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={temp_dir}") 
@@ -148,13 +139,14 @@ def scrape_data():
 
     # Initialize the WebDriver
     service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service,options=options) 
+    driver = webdriver.Chrome(service=service, options=options) 
 
     driver.get(st.session_state['url'])
 
-    #may need to define xpath for "i agree" button. Did not pop up for me, will try on someone elses device later
+    # may need to define xpath for "i agree" button. Did not pop up for me, will try on someone elses device later
     time.sleep(5) # Let initial 'Lite' page load
 
+    # --- ADDED INTERACTION & BACK SEQUENCE START ---
     try:
         # 1. Click on the screen to establish focus/interaction
         actions = ActionChains(driver)
@@ -171,69 +163,105 @@ def scrape_data():
         print(f"Interaction sequence failed: {e}")
     # --- ADDED INTERACTION & BACK SEQUENCE END ---
 
-    #obtain title
+    # obtain title
     # rest_name = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1').text
 
-    rest_name = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1').text
+    rest_name = driver.find_element(
+        By.XPATH, 
+        '/html/body/div[1]/div[2]/div[9]/div[10]/div/div/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[1]/h1'
+    ).text
 
-    #obtain resturant type
+    # obtain restaurant type
     # rest_type = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button').text
 
-    rest_type = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button').text
+    rest_type = driver.find_element(
+        By.XPATH,
+        '/html/body/div[1]/div[2]/div[9]/div[10]/div/div/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button'
+    ).text
 
-    #obtain restaurant value
+    # obtain restaurant value
     # value = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/span/span/span/span[2]/span/span').text
-    value = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/span/span/span/span[2]/span/span').text
-        #get restaurant address
-        #address = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[9]/div[3]/button/div/div[2]/div[1]").text
-        #can create a dictionary, key is state, div number is entry; search for state, if state, then that div number
+    # value = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div/div[1]/span/span/span/span[2]/span/span').text
 
-    #if need to go thru newer reviews first, insert that in here:
+    try:
+        value = driver.find_element(
+            By.XPATH, 
+            '/html/body/div[1]/div[2]/div[9]/div[10]/div/div/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div/div[1]/span/span/span/span[2]/span/span'
+        ).text
+    except NoSuchElementException:
+        value = None
 
-    #code chunk below helps us find review button. Got this code off of medium
+    # get restaurant address
+    # address = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[9]/div[3]/button/div/div[2]/div[1]").text
+    # can create a dictionary, key is state, div number is entry; search for state, if state, then that div number
+
+    # if need to go thru newer reviews first, insert that in here:
+
+    # code chunk below helps us find review button. Got this code off of medium
     driver.find_element(By.XPATH, "//button[contains(@aria-label, 'Reviews')]").click()
     time.sleep(5)
 
+    # QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde > div.PPCwl > div > div.jANrlb > div.fontDisplayLarge
 
-    #QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde > div.PPCwl > div > div.jANrlb > div.fontDisplayLarge
-     
-    #obtain rating
-    total_rating = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[2]/div/div[2]/div[1]').text
-    #scroll till all reviews are loaded up
-        #scroll by amount -- calculate and see how many reviews are in one scroll (10 scrolls is in one scroll)
-    num_reviews = driver.find_element(By.CSS_SELECTOR,'body > div:nth-child(5) > div.lbMcOd.y2iKwd.cSgCkb.xcUKcd.y2Sqzf > div.UL7Qtf > div.g2LZJb > div > div > div.w6VYqd > div.bJzME.DsGVEb.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde > div.PPCwl > div > div.jANrlb > div.fontBodySmall').text.split(" ")[0] #this code gives us the number
+    # obtain rating
+    total_rating = driver.find_element(
+        By.XPATH, 
+        '/html/body/div[1]/div[2]/div[9]/div[10]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[2]/div/div[2]/div[1]'
+    ).text
 
-    #some reviews may have columns, will want to take that out
+    # scroll till all reviews are loaded up
+    # scroll by amount -- calculate and see how many reviews are in one scroll (10 scrolls is in one scroll)
+
+    num_reviews = driver.find_element(
+        By.CSS_SELECTOR,
+        'body > div:nth-child(5) > div.lbMcOd.xcUKcd.y2Sqzf.y2iKwd.cSgCkb.Nkjr6c.K1N2o > div.UL7Qtf > div.g2LZJb > div > div > div.w6VYqd > div.bJzME.DsGVEb.IJUlqd.vJk0Jb > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.XiKgde.dS8AEf > div.PPCwl > div > div.jANrlb > div.fontBodySmall'
+    ).text.split(" ")[0] # this code gives us the number
+
+    # some reviews may have columns, will want to take that out
     if num_reviews.find(",") != -1:  # Check if comma is found
         num_reviews = num_reviews.replace(",", "")
     else:
         num_reviews = num_reviews
 
-
-    #now that we have number of reviews, we can scroll through reviews and load up each review
+    # now that we have number of reviews, we can scroll through reviews and load up each review
 
     height = 0
     while height <= (int(num_reviews)):
         try:
-            # scroll_element = driver.find_element(By.XPATH, "//*[@id='QA0Szd']/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]") #want to scroll first; finds scroll bar element
-            scroll_element = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]") #want to scroll first; finds scroll bar element
-            
-            try: #find "more"
-                more_element = driver.find_element(By.XPATH, "//button[@aria-label='See more']") 
+            scroll_element = driver.find_element(
+                By.XPATH,
+                '/html/body/div[1]/div[2]/div[9]/div[10]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]'
+            ) # want to scroll first; finds scroll bar element
+
+            try: # find "more"
+                more_element = driver.find_element(
+                    By.XPATH, 
+                    "//button[@aria-label='See more']"
+                )
+
                 if more_element.get_attribute("aria-expanded") == "false":
                     more_element.click()
-                    time.sleep(.20) #might try (int(num_reviews)/10) (would need to divide by the number of 0's plus 2) len(str(num_reviews)).
-            except NoSuchElementException: #scroll if no "more"
-                driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_element)
+                    time.sleep(.20)
+
+            except NoSuchElementException: # scroll if no "more"
+                driver.execute_script(
+                    "arguments[0].scrollTop = arguments[0].scrollHeight",
+                    scroll_element
+                )
                 time.sleep(.20)
+
         except NoSuchElementException:
             print("Scrollbar element not found.")
             break
-        height += 1 #once height is reached... or it doesnt touch anymore, break
+
+        height += 1 # once height is reached... or it doesnt touch anymore, break
     
-    #acquire reviews and parse them into a dataset #obtained from medium: https://medium.com/@isguzarsezgin/scraping-google-reviews-with-selenium-python-23135ffcc331
-    reviews = BeautifulSoup(driver.page_source,'html.parser')
+    # acquire reviews and parse them into a dataset
+    # obtained from medium:
+    # https://medium.com/@isguzarsezgin/scraping-google-reviews-with-selenium-python-23135ffcc331
+    reviews = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
+
     return rest_name, rest_type, value, total_rating, num_reviews, reviews
 
 if __name__ == "__main__":
@@ -857,4 +885,3 @@ def What_to_Expect():
 
 menu = st.navigation([Word_Analysis, Topic_Clustering, Mentions, What_to_Expect],position = 'sidebar',expanded = True)
 menu.run()
-
